7c34db3eadbc37c71453e624a3e056cad5e95d9f
root <root@DESKTOP-A0T568A.localdomain>
2022-11-20 18:33:37 +0530

    test_examples

--- test_examples.py
+++ test_examples.py
@@ -2,15 +2,14 @@ import math
 import numpy as np
 
 def test_sqrt():
-    num = 25
-    assert math.sqrt(num) == 5, "Sqrt failed"
+    num = 36
+    assert math.sqrt(num) == 6, "Sqrt failed"
 
 
 def test_square():
-    num = 4
-    assert num**2 == 16, "Square failed"
+    num = 5
+    assert num**2 == 25, "Square failed"
 
 
 def test_equality():
-    assert 10 == 10, "not equal"
-
+    assert 20 == 20, "not equal"
